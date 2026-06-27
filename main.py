c51ca21e784d9da61805cc7932e619a963bd4744
Varsha Tewatia <varshatewatia11@gmail.com>
2026-06-27 17:12:51 +0530

    Added password score and Suggestions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("=== Password Strength Checker ===")
+print("=== Password Strength Checker v1.1 ===")
 
 password = input("Enter your password: ")
 
@@ -20,12 +20,31 @@ special = "!@#$%^&*()_+-=[]{}|;:',.<>?/"
 
 if any(char in special for char in password):
     score += 1
+print("="*20)
+print(f"Password Score: {score}/5")
+print("="*20)
+
+print("\nSuggestions to improve your password:")
+if len(password) < 8:
+    print("- Use at least 8 characters.")
+if not any(char.isupper() for char in password):
+    print("- Include at least one uppercase letter.")
+if not any(char.islower() for char in password):
+    print("- Include at least one lowercase letter.")
+if not any(char.isdigit() for char in password):
+    print("- Include at least one digit.")
+if not any(char in special for char in password):
+    print("- Include at least one special character.")
 
 print()
 
 if score == 5:
-    print("Strong Password 💪")
+    print("Very Strong Password 💪")
+
 elif score >= 3:
-    print("Medium Password 👍")
+    print("Strong Password 👍")
+elif score == 2:
+    print("Medium Password 👌")
 else:
-    print("Weak Password ❌")
+    print("Weak password ❌")
+print("\nMade by Varsha Tewatia")
